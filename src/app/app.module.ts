8df65aef50821a0fe14bbaffcd867f63a1c8321f
chaman102 <chaman.adit24x7@gmail.com>
2022-04-19 21:08:36 +0530

    Updating Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MoviesComponent,
+    NotfoundComponent,
+    SliderComponent,
+    ItemsBannerComponent,
+    MovieItemComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,20 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../../models/movie';
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+  animations: [trigger('slideFade', [state('void', style({ opacity: 0 })), transition('void <=> *', [animate('1s')])])]
+})
+export class SliderComponent implements OnInit {
+  @Input() items: Movie[] = [];
+  currentSlideIndex: number = 0;
+  constructor() {}
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    }, 5000);
+  }
+}
